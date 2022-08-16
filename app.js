e197979b05ac271ea0a0cb1bd75fa1765b1a1cce
Agustina Stringa <agustinastringa1@gmail.com>
2022-08-16 20:54:22 -0300

    Incorporación de array

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-let producto = prompt('¿Que producto desea comprar? \n1. Cubre Grip \n2. Protector de paleta \n3. Funda para paleta \n99. Salir');
-while (producto != '99'){
-    let precio;
+const productos = [];
+let continuar = 'S';
+while (continuar == 'S'){
+    let producto = prompt('¿Que producto desea comprar? \n1. Cubre Grip \n2. Protector de paleta \n3. Funda para paleta');
     switch(producto){
         case '1':
-            precio = 500;
-            calcularCuotas(precio);
+            productos.push({producto: 'Cubre Grip', precio: 500});
             break;
         case '2':
-            precio = 1000;
-            calcularCuotas(precio);
+            productos.push({producto: 'Protector de paleta', precio: 1000});
             break;
         case '3':
-            precio = 2500;
-            calcularCuotas(precio);
+            productos.push({producto: 'Funda para paleta', precio: 2500});
             break;
         default:
             alert('Producto no reconocido')
             break;
     }
-    producto = prompt('¿Que producto desea comprar? \n1.Cubre Grip \n2. Protector de paleta \n3. Funda para paleta \n99. Salir');
+    continuar = prompt('¿Desea elegir otro producto? \nS. Si')
 }
+let total = 0;
+for (const prod of productos){
+    total = total + prod.precio;
+}
+calcularCuotas(total);
 
-function calcularCuotas(precio){
-    porcentaje = precio * 0.15;
+function calcularCuotas(total){
+    porcentaje = total * 0.15;
     tresCuotas = porcentaje * 3;
     tresTotal = tresCuotas *3;
     seisCuotas = porcentaje * 6;
     seisTotal = seisCuotas * 6;
-    cuotas = prompt('En cuantas cuotas desea pagarlo? \n1. 1 x $' + precio + ' ($' + precio + ') \n3. 3 x $' + tresCuotas + ' ($' + tresTotal + ') \n6. 6 x $' + seisCuotas + ' ($' + seisTotal + ') \n99. Salir');
+    cuotas = prompt('En cuantas cuotas desea pagarlo? \n1. 1 x $' + total + ' ($' + total + ') \n3. 3 x $' + tresCuotas + ' ($' + tresTotal + ') \n6. 6 x $' + seisCuotas + ' ($' + seisTotal + ') \n99. Salir');
     while(cuotas != '99'){
         switch(cuotas){
             case '1':
@@ -41,7 +44,7 @@ function calcularCuotas(precio){
                 break;
             default:
                 alert('Opción incorrecta');
-                cuotas = prompt('En cuantas cuotas desea pagarlo? \n1. 1 x $' + precio + ' ($' + precio + ') \n3. 3 x $' + tresCuotas + ' ($' + tresTotal + ') \n6. 6 x $' + seisCuotas + ' ($' + seisTotal + ') \n99. Salir');
+                cuotas = prompt('En cuantas cuotas desea pagarlo? \n1. 1 x $' + total + ' ($' + total + ') \n3. 3 x $' + tresCuotas + ' ($' + tresTotal + ') \n6. 6 x $' + seisCuotas + ' ($' + seisTotal + ') \n99. Salir');
                 break;
         }
         alert('Vuelva pronto!!');
